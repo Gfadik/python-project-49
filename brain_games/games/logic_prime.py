84c1b16b50ee1a03406edfc18a26f74d4c181077
Gfadik <oshkinis47gmail.com>
2025-04-01 16:30:26 +0300

    9_not_full

---
+++ brain_games/games/logic_prime.py
@@ -0,0 +1,12 @@
+import random
+import math
+
+
+rule = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+def processing_game():
+    question = random.randint(1,100)
+    
+
+
+    return question,
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,10 @@
+from brain_games.games import logic_prime
+from brain_games.engine import engine_in_games
+
+
+def main():
+	engine_in_games(logic_prime)
+
+
+if __name__ == '__main__':
+	main()
