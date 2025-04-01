9176d7ba832699949fda0e96de5d4d1c94638385
Gfadik <oshkinis47@gmail.com>
2025-04-01 18:30:42 +0300

    log

--- brain_games/games/logic_prime.py
+++ brain_games/games/logic_prime.py
@@ -6,7 +6,9 @@ rule = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def processing_game():
     question = random.randint(1,100)
-    x = 10
-
-
-    return question,
+    prime_number_check = math.gcd(question)
+    if prime_number_check == question:
+        correct_answer = yes
+    else:
+        correct_answer = no
+    return question, correct_answer
