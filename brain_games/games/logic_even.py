c2df6daf82de18053a55bc16806881e5ff88be54
Gfadik <oshkinis47gmail.com>
2025-04-02 10:38:28 +0300

    end_9_step

--- brain_games/games/logic_even.py
+++ brain_games/games/logic_even.py
@@ -4,10 +4,10 @@ rule = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def processing_game():
-	number = random.randint(1, 100)
-	check_answer = number % 2
+	question = random.randint(1, 100)
+	check_answer = question % 2
 	if check_answer == 0:
 		correct_answer = 'yes'
 	else:
 		correct_answer = 'no'
-	return number, correct_answer
+	return question, correct_answer
