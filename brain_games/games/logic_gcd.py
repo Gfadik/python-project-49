b4888bcc872d3e3c8d4328d734001abd33529bf5
Gfadik <oshkinis47gmail.com>
2025-04-03 13:27:12 +0300

    end_9_step

--- brain_games/games/logic_gcd.py
+++ brain_games/games/logic_gcd.py
@@ -7,6 +7,6 @@ rule = "Find the greatest common divisor of given numbers."
 def processing_game():
     number_first = random.randint(1, 100)
     number_second = random.randint(1, 100)
-    question = f'{number_first}  {number_second}'
+    question = f'{number_first} {number_second}'
     correct_answer = math.gcd(number_first, number_second)
     return question, str(correct_answer)
