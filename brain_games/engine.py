28c3c5dfd77b10f5f4826c643680c1985b07a6fa
Gfadik <oshkinis47gmail.com>
2025-04-03 10:31:37 +0300

    end_9_step

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -2,13 +2,13 @@ import prompt
 
 
 def engine_in_games(games):
-    name = prompt.string('May I have your name?')
+    name = prompt.string('May I have your name? ')
     print(f'Hello, {name}')
     print(f'{games.rule}')
     i = 1
     for i in range(3):
         task, correct_answer = games.processing_game()
-        print(f'Question:{task}')
+        print(f'Question: {task}')
         users_answer = prompt.string('Your answer: ')
         if correct_answer == users_answer:
             print('Correct!')
