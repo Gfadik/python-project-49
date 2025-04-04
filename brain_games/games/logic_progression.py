67ba4cef112081f5ece4cc7c82d83270aec096bc
Gfadik <oshkinis47gmail.com>
2025-04-04 14:42:36 +0300

    end_9_step

--- brain_games/games/logic_progression.py
+++ brain_games/games/logic_progression.py
@@ -1,17 +1,17 @@
 import random
 
-
 rule = 'What number is missing in the progression?'
+
 
 def processing_game():
     first_number = random.randint(1, 10)
     the_number_for_progression = random.randint(1, 10)
-    random_index_progression = random.randint(0,9)
+    random_index_progression = random.randint(0, 9)
     progression = []
     for i in range(10):
         progression.append(first_number)
         first_number += the_number_for_progression
     correct_answer = progression[random_index_progression]
     progression[random_index_progression] = '..'
-    question = ' '.join(map(str,progression))
+    question = ' '.join(map(str, progression))
     return question, str(correct_answer)
